caa8644a02e46da54297f16431300a83b4080999
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-12-22 05:00:42 -0600

    ☕️ update promp script

--- src/musicscript/__init__.py
+++ src/musicscript/__init__.py
@@ -1,74 +1,74 @@
 import os
+import requests
 from musicscript.album import Album
-from musicscript.utils import download
-from musicscript.utils import rename_songs
-from musicscript.utils import add_metadata
+from musicscript.download import download
+from musicscript.metadata import write_metadata
+from musicscript.metadata import add_songs
 
 
 def main() -> None:
     print("👾 Hello, Music Script")
 
-    # [0]: USER prompt
-    artist = input("Enter Artist Name: ")
+    # [0]: Get user inputs
+    album_artist = input("Enter Artist Name: ")
     album_name = input("Enter Album Name: ")
     url = input("Enter Soundcloud / Youtube Playlist: ")
     cover = input("Enter Image: ")
     year = input("Enter Year: ")
-    copyright = input("Enter Copyright: ")
+    copyright_ = input("Enter Copyright: ")
     genre = input("Enter Genre: ")
-    comment = input("Enter Comment: ")
-    description = input("Enter Description: ")
-    grouping = input("Enter grouping: ")
+    
+    # Example
+    # album_artist = "SZA"
+    # album_name = "SOS Deluxe: LANA"
+    # url = "https://www.youtube.com/watch?v=wk9kw_k4KNI&list=OLAK5uy_ke8Vb8WmKdgynHjSa7DSPguT58YKEEABQ&index=2"
+    # cover = "/Users/meekmsaki/Downloads/sza_lana_cover_1_zujeyx-4125153700-2.jpg"
+    # year = "2024"
+    # copyright_ = "℗ 2024 Top Dawg Entertainment, under exclusive license to RCA Records"
+    # genre = "R&B/SOUL"
 
-    # [0]: DEFAULT values
-    if not artist:
-        artist = "LUCKI"
-        print("💿 Using default artist:", artist)
+    # [1] Test url reponse
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+          raise ValueError(f"Request failed for url:{url}")
+    except:
+        raise ValueError(f"Request failed for url:{url}")
 
-    if not album_name:
-        album_name = "GEMINI!"
-        print("💿 Using default album:", album_name)
-
-    if not url:
-        url = "https://www.youtube.com/watch?v=-_CXWQQIOnQ&list=OLAK5uy_nOYJu0d8SCcr6K9n_0cJFEwG9WwjtyJQk"
-        print("💿 Using default url:", url)
-
-    # [0]: Populate Album
-    album = Album()
-    album.artist = artist
-    album.album_artist = artist
-    album.album = album_name
-    album.url = url
-    album.cover = cover
-    album.path = "music/" + album.artist + " - " + album.album
-    album._download_path = album.path + "/download"
+    # [2]: Create Album
+    album = Album(
+        url=url, album_artist=album_artist, album_name=album_name, cover=cover
+    )
     album.year = year
-    album.copyright = copyright
+    album.copyright = copyright_
     album.genre = genre
-    album.comment = comment
-    album.description = description
-    # album.grouping = grouping
 
     print("📦 Album Directory: ", album.path)
 
-    # [1]: Download songs
-    download(album, log=True)
+    # [3]: Download songs
+    download(album, log=False)
 
-    # [2]: Rename songs
-    rename_songs(album, log=True)
+    # [4]: Rename songs
+    add_songs(album=album, log=True)
 
-    # [3]: Add Artist metadata
-    add_metadata(album, log=True)
+    # [5]. Write metadata
+    write_metadata(album, log=True)
+
+    for song in album.songs:
+        assert os.path.isfile(
+            song.path
+        ), f"[Song Path Error]: Can't find added song {song.path}"
 
     # [4]: Print Results
     print("🗂️ Job Completed: Open", os.getcwd() + "/" + album.path)
-    print("🔗 Url:", album.url)
-    print("💿 Album:", album.album)
-    print("💿 Artist:", album.artist)
+    print("💿 Album:", album.album_name)
+    print("💿 Artist:", album.album_artist)
     print("💿 Cover:", album.cover)
     print("💿 Year:", album.year)
     print("💿 Genre:", album.genre)
     print("💿 Copyright:", album.copyright)
+    print("💿 Songs:", len(album.songs))
+    print("🔗 Url:", album.url)
 
 
 if __name__ == "__main__":
