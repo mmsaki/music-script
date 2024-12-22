a622dffe3468807c9296f9bf8ad89dffd64bf124
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-12-21 23:06:35 -0600

    📁 update album data structure

--- src/musicscript/album.py
+++ src/musicscript/album.py
@@ -1,51 +1,24 @@
+from dataclasses import dataclass
 from typing import List
+from musicscript.song import Song
 
 
+@dataclass
 class Album:
     """Class that describes an Album"""
 
-    _songs: List[str] = []
-    _download_path: str = ""
+    album_artist: str = ""
+    album_name: str = ""
+    year: str = ""
+    genre: str = ""
+    cover: str = ""
+    path: str = ""
+    url: str = ""
+    copyright: str = ""
+    download_path: str = ""
 
-    def __init__(
-        self,
-        title: str = "",
-        artist: str = "",
-        album_artist: str = "",
-        album: str = "",
-        grouping: str = "",
-        composer: str = "",
-        year: str = "",
-        track: str = "",
-        comment: str = "",
-        genre: str = "",
-        copyright: str = "",
-        performer: str = "",
-        description: str = "",
-        synopsis: str = "",
-        lyrics: str = "",
-        cover: str = "",
-        path: str = "",
-        url: str = "",
-    ):
-        self.title = title
-        self.artist = artist
-        self.album_artist = album_artist
-        self.album = album
-        self.grouping = grouping
-        self.composer = composer
-        self.year = year
-        self.track = track
-        self.comment = comment
-        self.genre = genre
-        self.copyright = copyright
-        self.description = description
-        self.synopsis = synopsis
-        self.performer = performer
-        self.lyrics = lyrics
-        self.cover = cover
-        self.path = path
-        self.url = url
+    def __init__(self):
+        self.songs = []
 
-    def songs(self) -> List[str]:
-        return self._songs
+    def add_song(self, song: Song):
+        self.songs.append(song)
