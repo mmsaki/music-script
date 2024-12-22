5a793035551ca0f840235d9ec54f43ef5d0f9e6c
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-12-22 03:53:20 -0600

    🪼 add defualt values for song

--- src/musicscript/song.py
+++ src/musicscript/song.py
@@ -1,12 +1,4 @@
-from typing import Union
-from dataclasses import dataclass
-
-
-@dataclass
 class Song:
-    title: str = ""
-    artist: str = ""
-    track: Union[int, str] = ""
     lyrics: str = ""
     genius_url: str = ""
     performer: str = ""
@@ -17,3 +9,14 @@ class Song:
     description: str = ""
     synopsis: str = ""
     path: str = ""
+    album_artist: str = ""
+    album_name: str = ""
+    year: str = ""
+    genre: str = ""
+
+    def __init__(self, artist: str, title: str, ext: str, cover: str, track: str = ""):
+        self.artist = artist
+        self.title = title
+        self.ext = ext
+        self.cover = cover
+        self.track = track
